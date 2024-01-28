402bce5db55537adccc7c81bdb70b8dc273ef397
Eranmonnie <ajalaferanmi123@gmail.com>
2024-01-28 11:19:41 +0100

    template engine

---
+++ engine.ts
@@ -0,0 +1,44 @@
+const tempEngine = (template: string, data: Record<string, any>): string => {
+  //regular expression for detecting out template tags
+  const reg: RegExp = /<%([^%>]+)?%>/g;
+  //regular expession for detecting conditions and loops
+  const reExp: RegExp = /(^( )?(if|for|else|switch|case|break|{|}))(.*)?/g;
+  //initial code variable wrapped in a string
+  let code: string = "var r=[];\n";
+  //cursor variable used to navigate through out html
+  let cursor: number = 0;
+  //match variable used in the loop
+  let match: RegExpExecArray | null;
+  //add function that appends html chunks to the code variable in a js syntax
+  const add = (html: string, js: boolean): void => {
+    js
+      ? (code += html.match(reExp) ? html + "\n" : `r.push(${html});\n`)
+      : (code += `r.push("${html.replace(/"/g, '\\"')}");\n`);
+  };
+  //loop generates html chunks that are passed to the add function
+  while ((match = reg.exec(template))) {
+    add(template.slice(cursor, match.index), false);
+    add(match[1], true);
+    cursor = match.index + match[0].length;
+  }
+
+  add(template.substr(cursor, template.length - cursor), false);
+  //joins the contents of code together and returns it, making it actual executable code
+  code += 'return r.join("");';
+  console.log(code);
+  //allows code to be exceutable
+  return new Function(code.replace(/[\r\t\n]/g, "")).apply(data);
+};
+
+const temp: string = `hello my name is <%this.name%>, i am
+   <%for(let x in this.age){%>
+      <%x%>
+  <%}%>
+    years old`;
+
+console.log(
+  tempEngine(temp, {
+    name: "feranmi",
+    age: [20, 10, 30],
+  })
+);
